bb631b7f6e67733ef6fd7660f5118bce27a65a97
zhanghui <iron.zhang@foxmail.com>
2019-11-25 23:10:20 +0800

    update

--- zaplog/zlogger/logger_test.go
+++ zaplog/zlogger/logger_test.go
@@ -2,74 +2,168 @@ package zlogger
 
 import (
 	"context"
-	"os"
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 
 	"github.com/ironzhang/tlog/iface"
+	"github.com/ironzhang/tlog/zaplog/zbase"
 )
 
-type TestContextHook struct{}
-
-func (p TestContextHook) WithContext(ctx context.Context) []interface{} {
-	return []interface{}{"trace_id", "123456"}
+type TLogged struct {
+	entries []observer.LoggedEntry
 }
 
-func NewTestCore() zapcore.Core {
-	enc := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
-	return zapcore.NewCore(enc, os.Stdout, zapcore.DebugLevel)
+func (p *TLogged) Add(e zapcore.Entry, fields ...zapcore.Field) {
+	if len(fields) == 0 {
+		fields = []zapcore.Field{}
+	}
+	p.entries = append(p.entries, observer.LoggedEntry{e, fields})
 }
 
-func NewTestLogger() *Logger {
-	core := NewTestCore()
-	return New("", core, TestContextHook{}, zap.AddCaller())
+func NewTestLogger(t testing.TB, name string, level iface.Level, hook ContextHook,
+	opts ...zap.Option) (*Logger, *observer.ObservedLogs) {
+	core, logs := observer.New(zbase.ZapLevel(level))
+	return New(name, core, hook, opts...), logs
 }
 
 func TestLoggerPrint(t *testing.T) {
-	logger := NewTestLogger()
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "", iface.DEBUG, nil)
+
 	min, max := iface.DEBUG, iface.ERROR
 	for lvl := min; lvl <= max; lvl++ {
-		logger.Print(0, lvl, "Print level", lvl)
-		logger.Printf(0, lvl, "Printf level=%s", lvl)
-		logger.Printw(0, lvl, "Printw", "level", lvl)
+		logger.Print(0, lvl, "Print", lvl)
+		logger.Printf(0, lvl, "Print: %v", lvl)
+		logger.Printw(0, lvl, "Print", "level", lvl)
+
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(lvl), Message: fmt.Sprint("Print", lvl)})
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(lvl), Message: fmt.Sprintf("Print: %v", lvl)})
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(lvl), Message: "Print"}, zap.Any("level", lvl))
 	}
+
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
 }
 
-func TestLogger(t *testing.T) {
-	logger := NewTestLogger().WithArgs("function", "TestLogger").WithContext(context.Background())
+func TestLoggerLog(t *testing.T) {
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "", iface.DEBUG, nil)
 
-	type LogFunc func(args ...interface{})
-	logFuncs := []LogFunc{
-		logger.Debug,
-		logger.Info,
-		logger.Warn,
-		logger.Error,
+	tests := []struct {
+		level iface.Level
+		log   func(args ...interface{})
+		logf  func(format string, args ...interface{})
+		logw  func(message string, kvs ...interface{})
+	}{
+		{
+			level: iface.DEBUG,
+			log:   logger.Debug,
+			logf:  logger.Debugf,
+			logw:  logger.Debugw,
+		},
+		{
+			level: iface.INFO,
+			log:   logger.Info,
+			logf:  logger.Infof,
+			logw:  logger.Infow,
+		},
+		{
+			level: iface.WARN,
+			log:   logger.Warn,
+			logf:  logger.Warnf,
+			logw:  logger.Warnw,
+		},
+		{
+			level: iface.ERROR,
+			log:   logger.Error,
+			logf:  logger.Errorf,
+			logw:  logger.Errorw,
+		},
 	}
-	for _, log := range logFuncs {
-		log("hello", "world")
+	for _, tt := range tests {
+		tt.log("hello", "world")
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(tt.level), Message: "helloworld"})
+		tt.logf("%s, %s", "hello", "world")
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(tt.level), Message: "hello, world"})
+		tt.logw("greeting", "hello", "world")
+		logged.Add(zapcore.Entry{Level: zbase.ZapLevel(tt.level), Message: "greeting"}, zap.String("hello", "world"))
 	}
 
-	type LogfFunc func(format string, args ...interface{})
-	logfFuncs := []LogfFunc{
-		logger.Debugf,
-		logger.Infof,
-		logger.Warnf,
-		logger.Errorf,
-	}
-	for _, log := range logfFuncs {
-		log("hello, %s", "world")
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
+}
+
+func TestLoggerPanic(t *testing.T) {
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "", iface.DEBUG, nil)
+
+	assert.Panics(t, func() { logger.Panic("panic") }, "Expected panic")
+	assert.Panics(t, func() { logger.Panicf("%s", "panicf") }, "Expected panicf")
+	assert.Panics(t, func() { logger.Panicw("panicw", "hello", "world") }, "Expected panicw")
+
+	logged.Add(zapcore.Entry{Level: zapcore.PanicLevel, Message: "panic"})
+	logged.Add(zapcore.Entry{Level: zapcore.PanicLevel, Message: "panicf"})
+	logged.Add(zapcore.Entry{Level: zapcore.PanicLevel, Message: "panicw"}, zap.String("hello", "world"))
+
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
+}
+
+type TContextHook struct{}
+
+func (p TContextHook) WithContext(ctx context.Context) []interface{} {
+	return []interface{}{"trace_id", "123456"}
+}
+
+func TestLoggerWith(t *testing.T) {
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "", iface.DEBUG, TContextHook{})
+
+	logger.WithArgs("k1", "v1").Info()
+	logger.WithArgs("k2", "v2").Info()
+	logger.WithContext(context.Background()).Info()
+	logger.WithArgs("k3", "v3").WithContext(context.Background()).Info()
+	logger.WithContext(context.Background()).WithArgs("k4", "v4").Info()
+
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel}, zap.String("k1", "v1"))
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel}, zap.String("k2", "v2"))
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel}, zap.String("trace_id", "123456"))
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel}, zap.String("trace_id", "123456"), zap.String("k3", "v3"))
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel}, zap.String("trace_id", "123456"), zap.String("k4", "v4"))
+
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
+}
+
+func TestLoggerWithoutContext(t *testing.T) {
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "", iface.DEBUG, nil)
+
+	logger.WithContext(context.Background()).Info()
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel})
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
+}
+
+func TestLoggerName(t *testing.T) {
+	var logged TLogged
+	logger, logs := NewTestLogger(t, "name", iface.DEBUG, nil)
+
+	if got, want := logger.Name(), "name"; got != want {
+		t.Fatalf("logger name is %q, isn't %q", got, want)
 	}
 
-	type LogwFunc func(message string, kvs ...interface{})
-	logwFuncs := []LogwFunc{
-		logger.Debugw,
-		logger.Infow,
-		logger.Warnw,
-		logger.Errorw,
-	}
-	for _, log := range logwFuncs {
-		log("hello, world", "hello", "world")
-	}
+	logger.Info()
+	logged.Add(zapcore.Entry{Level: zapcore.InfoLevel, LoggerName: "name"})
+	assert.Equal(t, logged.entries, logs.AllUntimed(), "unexpected log entries")
+}
+
+func TestLoggerAddCaller(t *testing.T) {
+	logger, logs := NewTestLogger(t, "name", iface.DEBUG, nil, zap.AddCaller())
+
+	logger.Info()
+	output := logs.AllUntimed()
+	assert.Equal(t, 1, len(output), "unexpected number of logs written out")
+	assert.Regexp(t, `logger_test.go`, output[0].Caller.String(), "unexpected caller")
+	//assert.Equal(t, 164, output[0].Caller.Line, "unexpected line")
 }
