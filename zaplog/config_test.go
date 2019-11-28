badabbcd31c6001ba10cd062f37e0eee7f4fea62
zhanghui <iron.zhang@foxmail.com>
2019-11-28 10:33:31 +0800

    update

--- zaplog/config_test.go
+++ zaplog/config_test.go
@@ -112,3 +112,85 @@ func TestStacktraceLevelUnmarshal(t *testing.T) {
 		t.Logf("%d: stacktrace level: got %v", i, l)
 	}
 }
+
+/*
+func TestLevelEncoderZap(t *testing.T) {
+	tests := []struct {
+		e LevelEncoder
+		z zapcore.LevelEncoder
+	}{
+		{
+			e: CapitalLevelEncoder,
+			z: zapcore.CapitalLevelEncoder,
+		},
+	}
+	for i, tt := range tests {
+		z := tt.e.zap()
+		e := zapcore.NewMapObjectEncoder()
+		z(zapcore.DebugLevel, e)
+		if got, want := z, tt.z; !assert.Equal(t, got, want) {
+			t.Errorf("%d: zap: got %v, want %v", i, got, want)
+			continue
+		}
+	}
+}
+*/
+
+func TestLevelEncoderMarshal(t *testing.T) {
+	tests := []struct {
+		e LevelEncoder
+		s string
+	}{
+		{-1, "LevelEncoder(-1)"},
+		{CapitalLevelEncoder, "capital"},
+		{CapitalColorLevelEncoder, "capitalColor"},
+		{LowercaseLevelEncoder, "lowercase"},
+		{LowercaseColorLevelEncoder, "lowercaseColor"},
+	}
+	for i, tt := range tests {
+		text, err := tt.e.MarshalText()
+		if err != nil {
+			t.Errorf("%d: marshal text: %v", i, err)
+			continue
+		}
+		if got, want := string(text), tt.s; got != want {
+			t.Errorf("%d: text: got %v, want %v", i, got, want)
+			continue
+		}
+		t.Logf("%d: text: got %s", i, text)
+	}
+}
+
+func TestLevelEncoderUnmarshal(t *testing.T) {
+	tests := []struct {
+		s   string
+		e   LevelEncoder
+		err string
+	}{
+		{s: "LevelEncoder(-1)", err: "unrecognized level encoder"},
+		{s: "capital", e: CapitalLevelEncoder},
+		{s: "capitalColor", e: CapitalColorLevelEncoder},
+		{s: "lowercase", e: LowercaseLevelEncoder},
+		{s: "lowercaseColor", e: LowercaseColorLevelEncoder},
+		{s: "LOWERCASECOLOR", e: LowercaseColorLevelEncoder},
+		{s: "lowercasecolor", e: LowercaseColorLevelEncoder},
+		{s: "loWercasecolor", e: LowercaseColorLevelEncoder},
+	}
+	for i, tt := range tests {
+		var e LevelEncoder
+		err := e.UnmarshalText([]byte(tt.s))
+		if !matchError(t, err, tt.err) {
+			t.Errorf("%d: match error: got %v, want %v", i, err, tt.err)
+			continue
+		}
+		if err != nil {
+			t.Logf("%d: unmarshal text: %v", i, err)
+			continue
+		}
+		if got, want := e, tt.e; got != want {
+			t.Errorf("%d: level encoder: got %v, want %v", i, got, want)
+			continue
+		}
+		t.Logf("%d: level encoder: got %v", i, e)
+	}
+}
