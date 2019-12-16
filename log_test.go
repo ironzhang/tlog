f8a0c873a62822fd6cf30506abf66194b720be14
zhanghui <iron.zhang@foxmail.com>
2019-12-16 17:33:19 +0800

    update log test

--- log_test.go
+++ log_test.go
@@ -6,52 +6,88 @@ import (
 	"os"
 
 	"github.com/ironzhang/tlog"
+	"github.com/ironzhang/tlog/iface"
 	"github.com/ironzhang/tlog/zaplog"
 )
 
-func ExampleStdLogger() {
-	zaplog.StdContextHook = func(ctx context.Context) []interface{} {
-		return []interface{}{"TraceID", "123456"}
+func ContextTestHook(ctx context.Context) []interface{} {
+	return []interface{}{"TraceID", "123456"}
+}
+
+func RecoverPanic(f func()) {
+	defer func() {
+		recover()
+	}()
+	f()
+}
+
+func PrintLog(name string) {
+	min, max := iface.DEBUG, iface.PANIC
+	for lv := min; lv <= max; lv++ {
+		RecoverPanic(func() {
+			tlog.Print(0, lv, "Print ", lv, " ", name)
+		})
+		RecoverPanic(func() {
+			tlog.Printf(0, lv, "Printf %s %s", lv, name)
+		})
+		RecoverPanic(func() {
+			tlog.Printw(0, lv, "Printw", "level", lv, "name", name)
+		})
 	}
 
-	tlog.Debug("debug")
-	tlog.Info("info")
-	tlog.Warn("warn")
-	tlog.Error("error")
-	func() {
-		defer func() {
-			recover()
-		}()
-		tlog.Panic("panic")
-	}()
+	tlog.Debug("Debug ", name)
+	tlog.Debugf("Debugf %s", name)
+	tlog.Debugw("Debugw", "name", name)
 
-	tlog.WithArgs("function", "TestLog").Info("hello")
-	tlog.WithContext(context.Background()).WithArgs("function", "TestLog").Info("hello")
+	tlog.Info("Info ", name)
+	tlog.Infof("Infof %s", name)
+	tlog.Infow("Infow", "name", name)
+
+	tlog.Warn("Warn ", name)
+	tlog.Warnf("Warnf %s", name)
+	tlog.Warnw("Warnw", "name", name)
+
+	tlog.Error("Error ", name)
+	tlog.Errorf("Errorf %s", name)
+	tlog.Errorw("Errorw", "name", name)
+
+	RecoverPanic(func() {
+		tlog.Panic("Panic", name)
+	})
+	RecoverPanic(func() {
+		tlog.Panicf("Panicf %s", name)
+	})
+	RecoverPanic(func() {
+		tlog.Panicw("Panicw", "name", name)
+	})
+
+	tlog.WithArgs("name", name).Info("with args")
+	tlog.WithContext(context.Background()).Info("with context")
+	tlog.WithArgs("name", name).WithContext(context.Background()).Info("with args and with context")
+}
+
+func ExampleNopLogger() {
+	tlog.SetFactory(nil)
+	PrintLog("ExampleNopLogger")
+
+	// output:
+}
+
+func ExampleStdLogger() {
+	zaplog.StdContextHook = ContextTestHook
+	PrintLog("ExampleStdLogger")
 
 	// output:
 }
 
 func ExampleLogger() {
-	logger, err := zaplog.New(zaplog.NewDevelopmentConfig())
+	logger, err := zaplog.New(zaplog.NewDevelopmentConfig(), zaplog.SetContextHook(zaplog.ContextHookFunc(ContextTestHook)))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "new logger: %v", err)
 		return
 	}
 	tlog.SetFactory(logger)
-
-	tlog.Debug("debug")
-	tlog.Info("info")
-	tlog.Warn("warn")
-	tlog.Error("error")
-	func() {
-		defer func() {
-			recover()
-		}()
-		tlog.Panic("panic")
-	}()
-
-	tlog.WithArgs("function", "TestLog").Info("hello")
-	tlog.WithContext(context.Background()).WithArgs("function", "TestLog").Info("hello")
+	PrintLog("ExampleLogger")
 
 	// output:
 }
