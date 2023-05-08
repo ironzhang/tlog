6a961ec458ee41c7fe9e99d7b40cf0538d2788ed
roshilikang <roshilikang@didiglobal.com>
2023-05-08 15:58:19 +0800

    feat: 支持动态获取、修改日志级别接口

--- httpserve/log_level.go
+++ httpserve/log_level.go
@@ -1,28 +1,39 @@
-package tlog
+package httpserve
 
 import (
 	"io"
 	"net/http"
 
+	"git.xiaojukeji.com/pearls/tlog"
 	"git.xiaojukeji.com/pearls/tlog/iface"
 )
 
 func init() {
-	http.HandleFunc("/tlog/level/get", GetLogLevel)
-	http.HandleFunc("/tlog/level/set", SetLogLevel)
+	http.HandleFunc("/tlog/level", logLevel)
 }
 
-func GetLogLevel(w http.ResponseWriter, r *http.Request) {
+func logLevel(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getLogLevel(w, r)
+	case http.MethodPost, http.MethodPut:
+		setLogLevel(w, r)
+	default:
+		writeTextResponse(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
+	}
+}
+
+func getLogLevel(w http.ResponseWriter, r *http.Request) {
 	level := "unknown"
 	status := http.StatusOK
-	if gs, ok := GetLogger().(iface.GetSetLevel); ok {
+	if gs, ok := tlog.GetLogger().(iface.GetSetLevel); ok {
 		level = gs.GetLevel().String()
 		status = http.StatusInternalServerError
 	}
 	writeTextResponse(w, status, level)
 }
 
-func SetLogLevel(w http.ResponseWriter, r *http.Request) {
+func setLogLevel(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		writeTextResponse(w, http.StatusInternalServerError, "Unexpected Request Body")
@@ -35,7 +46,7 @@ func SetLogLevel(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	logger := GetLogger()
+	logger := tlog.GetLogger()
 	if gs, ok := logger.(iface.GetSetLevel); ok {
 		gs.SetLevel(level)
 		writeTextResponse(w, http.StatusOK, "OK")
